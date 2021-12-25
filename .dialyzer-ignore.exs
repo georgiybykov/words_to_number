08fe5d7aa4f02cfcc129b3348e207ca839e8dda4
Georgiy Bykov <zhora.bykov@gmail.com>
2021-12-25 21:30:22 +0300

    Add and configure dialyxir deps

---
+++ .dialyzer-ignore.exs
@@ -0,0 +1,3 @@
+# The list of the files to ignoge could be placed below.
+#   SEE: https://github.com/jeremyjh/dialyxir#elixir-term-format
+[]
--- mix.exs
+++ mix.exs
@@ -8,6 +8,12 @@ defmodule WordsToNumber.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit, :iex],
+        ignore_warnings: ".dialyzer-ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -24,6 +30,7 @@ defmodule WordsToNumber.MixProject do
 
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:credo_contrib, "~> 0.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
